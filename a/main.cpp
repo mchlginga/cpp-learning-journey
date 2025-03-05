cf19c914f11c5b63e67e3884b76344627efe7636
mchl <mchlginga@gmail.com>
2025-03-05 23:32:53 +0800

    Added Directory named 'a'

---
+++ a/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // preprocessor directives
+
+int main ()
+{ // main function
+
+    int x; // declarative statement
+
+    x = 1; // expression statement
+
+    return 0; // return statement
+}
